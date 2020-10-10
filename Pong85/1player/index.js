f51211206614eeb7f1116394e02d9947cb5e5212
GMCgit <goranivancic117@gmail.com>
2020-10-10 17:25:36 +0200

    Added in mobile support for pong

--- Pong85/1player/index.js
+++ Pong85/1player/index.js
@@ -7,13 +7,13 @@ let direction = { x: BALL_SPEED, y: BALL_SPEED };
 let keyMap = [];
 let ScoreLeft = 0,
   ScoreRight = 0;
-const HIT_SOUND = new Audio('../soundFX/HtiV2.wav');
+const HIT_SOUND = new Audio("../soundFX/HtiV2.wav");
 HIT_SOUND.volume = 0.5;
 
-const WIN_SOUND = new Audio('../soundFX/Powerup2.wav');
+const WIN_SOUND = new Audio("../soundFX/Powerup2.wav");
 WIN_SOUND.volume = 0.5;
 
-const LOOSE_SOUND = new Audio('');
+const LOOSE_SOUND = new Audio("");
 
 function init() {
   canvas = document.getElementById("gameBoard");
@@ -43,12 +43,73 @@ function init() {
   bot = new Bot(playerRight);
 
   bot.decideMovement();
+
+  startup();
+
   setTimeout(function () {
     clearInterval(intevalCountDown);
     gameLoop();
   }, 4000);
 }
 
+//? Mobile Support for movement
+function startup() {
+  document.addEventListener("touchstart", handleTouchStart, false);
+  document.addEventListener("touchmove", handleTouchMove, false);
+}
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* left swipe */
+    } else {
+      /* right swipe */
+    }
+  } else if (Math.abs(xDiff) < Math.abs(yDiff)) {
+    if (yDiff > 0) {
+      keyMap["s"] = false;
+      keyMap["w"] = true;
+    } else {
+      keyMap["w"] = false;
+      keyMap["s"] = true;
+    }
+  } else {
+    keyMap["w"] = false;
+    keyMap["s"] = false;
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
+//? Rest
 function gameLoop(currentTime) {
   window.requestAnimationFrame(gameLoop);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
@@ -78,9 +139,7 @@ function update() {
 
     ball.x += direction.x * 2;
     ball.y += direction.y * 2;
-  } else if (
-    RectCircleColliding(ball, playerLeft)
-  ) {
+  } else if (RectCircleColliding(ball, playerLeft)) {
     HIT_SOUND.pause();
     HIT_SOUND.currentTime = 0;
     HIT_SOUND.play();
